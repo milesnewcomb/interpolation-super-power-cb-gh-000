458ed33fa701aee1e80f62b6c50ba1a7c78e7382
Miles Newcomb <msnewcomb@student.fullsail.edu>
2018-12-17 02:21:16 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -8,4 +8,4 @@ def display_rainbow(colors)
   puts "V: #{colors[6]}"
 end
 
-colors = ["R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"]
+colors = ["R:", "O:", "Y:", "G:", "B:", "I:", "V:"]
